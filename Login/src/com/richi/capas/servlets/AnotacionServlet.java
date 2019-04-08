f3326b09de16a5b9092e331420dc6a5444bed257
urrut <urrut@DESKTOP-Goiiidooo>
2019-04-08 16:05:25 -0600

    1

---
+++ Login/src/com/richi/capas/servlets/AnotacionServlet.java
@@ -0,0 +1,56 @@
+package com.richi.capas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnotacionServlet
+ */
+@WebServlet("/AnotacionServlet")
+public class AnotacionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnotacionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String user="Richi";
+		String pass="0000";
+		String uservalue= request.getParameter("user");
+		String userpass = request.getParameter("pass");
+		
+		PrintWriter out = response.getWriter();
+		
+		if(user.equals(uservalue) && pass.equals(userpass)) {
+			out.println("<h1>" + "Bienvenido" +" </h1>");
+			
+		}else {
+			out.println("<h1>" + "Nel Prriwini" + "</h1>");
+		}
+	}
+
+}
